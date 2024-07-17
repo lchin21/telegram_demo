0732e3c6fa11487c7ce550b5ce3b736e86805970
davispham1 <davispham1126@gmail.com>
2024-07-17 12:06:19 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,31 +38,29 @@ export default function App({ Component, pageProps }: AppProps) {
   //   </RainbowKitProvider>
   // </WagmiConfig>
 
-  return
-  <WagmiConfig client={wagmiConfig}>
-    <RainbowKitProvider chains={chains}>
-      <ModalProvider options={{
-        projectId: "ac297642-d52d-46dc-9437-2afafdc87edf",
-        clientKey: "cTHMhkM3NSaoZNYWOgz1USNAxqXRRfxkrfN8NlMn",
-        appId: "468d50a2-a253-49c8-82b8-8647f682bed1",
-        chains: [EthereumSepolia],
-        connectors: [
-          ...evmWallets({ projectId: process.env.REACT_APP_WALLETCONNECT_ID, showQrModal: true }),
-          ...solanaWallets(),
-        ],
-        // erc4337: { //optional: account abstraction wallet UI config (displaying the smart account rather than EOA)
-        //   name: "SIMPLE",
-        //   version: "1.0.0"
-        // },
-        wallet: { //optional: particle wallet config
-          customStyle: {
-            supportChains: [EthereumSepolia],
-          },
-        },
-      }}>
-        <Component {...pageProps} />
-      </ModalProvider>
-    </RainbowKitProvider>
-  </WagmiConfig>
-
+  return (
+    <WagmiConfig client={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <ModalProvider
+          options={{
+            projectId: "ac297642-d52d-46dc-9437-2afafdc87edf",
+            clientKey: "cTHMhkM3NSaoZNYWOgz1USNAxqXRRfxkrfN8NlMn",
+            appId: "468d50a2-a253-49c8-82b8-8647f682bed1",
+            chains: [EthereumSepolia],
+            connectors: [
+              ...evmWallets({ projectId: process.env.REACT_APP_WALLETCONNECT_ID, showQrModal: true }),
+              ...solanaWallets(),
+            ],
+            wallet: {
+              customStyle: {
+                supportChains: [EthereumSepolia],
+              },
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </ModalProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
